8dbab31a2ef4511020a8e2d583fd96b49e810fac
deepalimittal <jyotsnamittal@Jyotsna-Mittals-iMac.local>
2015-07-12 13:24:46 -0700

    first commit

---
+++ evenNumberList.playground/section-1.swift
@@ -0,0 +1,50 @@
+
+//var number = 2
+//while number < 21{
+///    println("\(number)")
+//    number += 2
+//}
+
+
+//var number = 31
+//if (number % 2 == 0){
+//    println("even")
+//} else{
+//    println("odd")
+//}
+
+
+//var num = 3
+//var number = 3
+//if (number < num){
+//    println("\(num)")
+//} else if(num < number){
+//    println("\(number)")
+//} else if(number == num){
+//    println("none")
+//}
+
+
+//var number = -8.0
+//if (number >= 4.5) || (number < 0.0){
+//    println("Invalid")
+//}else if (number >= 4.0){
+//    println("A")
+//}else if (number >= 3.3){
+//    println("B")
+//}else if (number >= 2.0){
+//    println("C")
+//}else if (number >= 1.0){
+//    println("D")
+//}else if (number < 1.0){
+//    println("F")
+//}
+
+var n = 5
+for index1 in 1...n{
+    println()
+
+for index2 in 1...index1 {
+    print("*")
+    }
+}
